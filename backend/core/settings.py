05d88e009bcd8e26c3155a3a00698832b8eff2bc
DiegoH3nr1que <diegohenrique123012@gmail.com>
2024-08-22 21:53:44 -0300

    refactor(): modifications to the project configuration so that the frontend can access the api

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,6 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Substitua pelo domínio do seu frontend
+    "https://seufrontend.com",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 
 # Application definition
 
@@ -38,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    'corsheaders',
     "sensors"
 ]
 
@@ -46,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
